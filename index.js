8a387750bb65d0c7242cba30e87d1b22379a7175
xlegendario <dario.b1996@gmail.com>
2026-05-13 22:28:22 +0200

    Update index.js

--- index.js
+++ index.js
@@ -454,15 +454,15 @@ app.get("/api/orders", async (req, res) => {
         invoice_price: moneyValue(f["Invoice Price (VAT Included)"]),
         vat_type: (() => {
           const originalVat = displayValue(f["VAT Type"]);
-          const country = displayValue(f["Client Country"]).toLowerCase();
+          const country = displayValue(f["Client Country"])
+            .toLowerCase()
+            .trim();
         
-          if (originalVat === "Margin") {
-            return "Margin";
-          }
+          if (originalVat === "Margin") return "Margin";
         
-          return country === "netherlands"
-            ? "VAT21"
-            : "VAT0";
+          if (country.includes("netherlands")) return "VAT21";
+        
+          return "VAT0";
         })(),
 
         fulfillment_status: displayValue(f["Fulfillment Status"]),
